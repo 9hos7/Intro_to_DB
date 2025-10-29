f8992f44c2510ff0f2073e2a613c3b685fbf3f59
Dilinna <anoziedilinna12@gmail.com>
2025-10-29 23:46:04 +0100

    initial commit

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+-- USE THE DATABASE
+USE alx_book_store;
+
+
+-- SCRIPT TO INSERT A SINGLE ROW INTO THE CUSTOMERS TABLE
+
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
