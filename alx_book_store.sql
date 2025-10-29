854bb6ee7cd9539f0da8eb3bbb4d262a11274415
Dilinna <anoziedilinna12@gmail.com>
2025-10-29 23:12:02 +0100

    initial commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -17,7 +17,7 @@ CREATE TABLE Books (
     author_id INT NOT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
@@ -35,7 +35,7 @@ CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
@@ -46,10 +46,10 @@ CREATE TABLE Order_Details (
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
